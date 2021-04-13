2cff7b5362bbeac19beb1bb240614cd10030a23a
samuelecisaro <samuele.cisaro@hotmail.it>
2021-04-14 01:02:34 +0200

    bugfix: tipyng error

--- src/block.js
+++ src/block.js
@@ -17,7 +17,7 @@ class Block {
     // Constructor - argument data will be the object containing the transaction data
 	constructor(data){
 		this.hash = null;                                           // Hash of the block
-		this.height = 0;                                            // Block Height (consecutive number of each block)
+		this.heigth = 0;                                            // Block Height (consecutive number of each block)
 		this.body = Buffer(JSON.stringify(data)).toString('hex');   // Will contain the transactions stored in the block, by default it will encode the data
         this.time = 0;                                                  // Timestamp for the Block creation
         this.star_owner = null;                                     // wallet address of the star owner
